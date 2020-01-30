5a0ff114f64a0b9db95b6ddad393f33dc86a57db
tsai <tsai@GJ437b.local>
2020-01-30 20:17:44 +0000

    change cfg HD189 sflux

--- vulcan_cfg.py
+++ vulcan_cfg.py
@@ -14,7 +14,7 @@ atm_file = 'atm/atm_HD189_Kzz.txt'
 sflux_file = 'atm/stellar_flux/sflux-HD189_Moses11.txt' # This is the flux density at the stellar surface
 top_BC_flux_file = 'atm/BC_top.txt'
 bot_BC_flux_file = 'atm/BC_bot.txt'
-vul_ini = 'output/moses_HD189.vul'
+vul_ini = 'output/'
 # output:
 output_dir = 'output/'
 plot_dir = 'plot/'
@@ -35,23 +35,24 @@ const_mix = {'CH4':2.7761E-4*2, 'O2':4.807e-4, 'He':0.09691, 'N2':8.1853E-5, 'H2
 # ====== Setting up photochemistry ======
 use_photo = True
 # astronomy input
-r_star = 0.752 # stellar radius in solar radius
+r_star = 0.805 # stellar radius in solar radius
 orbit_radius = 0.03142 # planet-star distance in A.U.
 sl_angle = 48 /180.*3.14159 # the zenith angle of the star in degree
 # radiation parameters 
-excit_sp = ['O_1', 'CH2_1'] # N_D not included due to lack of NASA9 Gibbs energy
+excit_sp = ['O_1', 'CH2_1', 'N_2D'] # N_2D not included due to lack of NASA9 Gibbs energy
 scat_sp = ['H2', 'He'] # the bulk compositions that contribute to Rayleigh scattering
-edd = 0.5  # the Eddington coefficient 
-dbin = 0.2  # the uniform bin width
+atom_sp = ['H'] # Na and K? # atoms that do not participate in photodissociation but contribute to the absorption
+edd = 0.1 # the Eddington coefficient 
+dbin = 0.1 # the uniform bin width
 # frequency to update the flux and optical depth
 ini_update_photo_frq = 100
-final_update_photo_frq = 10
+final_update_photo_frq = 5
 
 # ====== Setting up parameters for the atmosphere ======
 atm_base = 'H2' #Options: 'H2', 'N2', 'O2', 'CO2 -- the bulk gas of the atmosphere: affects molecular diffsion
-nz = 120   # number of vertical layers
-P_b = 1.E9 # pressure at the bottom (dyne/cm^2)
-P_t = 1.e-2 # pressure at the top (dyne/cm^2)
+nz = 150   # number of vertical layers
+P_b = 1e9 #1.E9 # pressure at the bottom (dyne/cm^2)
+P_t = 1.e-2 #1.e-2 # pressure at the top (dyne/cm^2)
 use_Kzz = True
 use_moldiff = True
 use_vz = False
@@ -59,12 +60,13 @@ atm_type = 'file' # Options: 'isothermal', 'analytical', or 'file'
 Kzz_prof = 'file' # Options: 'const' or 'file'
 vz_prof = 'const' # Options: 'const' or 'file'
 g = 2140.         # gravity (cm/s^2)  (HD189:2140  HD209:936)
-Tiso = 3000. # only reads when atm_type = 'isothermal'
+Tiso = 5000. # only reads when atm_type = 'isothermal'
 # setting the parameters for the analytical T-P from (126)in Heng et al. 2014. Only reads when atm_type = 'analytical' 
 # T_int, T_irr, ka_L, ka_S, beta_S, beta_L
 para_warm = [120., 1500., 0.1, 0.02, 1., 1.]
+para_cool = [90.,  1000., 0.04, 0.01, 1., 1.]
 para_anaTP = para_warm
-const_Kzz = 1.E9 # (cm^2/s) Only reads when use_Kzz = True and Kzz_prof = 'const'
+const_Kzz = 1.E10 # (cm^2/s) Only reads when use_Kzz = True and Kzz_prof = 'const'
 const_vz = 0 # (cm/s) Only reads when use_vz = True and vz_prof = 'const'
 
 # frequency for updating dz and dzi due to change of mu
@@ -75,7 +77,7 @@ update_frq = 100
 use_topflux = False
 use_botflux = False
 #use_fix_all_bot = True
-use_fix_sp_bot = False 
+use_fix_sp_bot = {}
 
 # ====== Reactions to be switched off  ======
 remove_list = [] # in pairs e.g. [1,2]
@@ -83,7 +85,7 @@ remove_list = [] # in pairs e.g. [1,2]
 # == Condensation (Ongoing testing!)  ======
 use_condense = False
 use_settling = False
-#start_conden_time = 1e10
+start_conden_time = 1e10
 condesne_sp = ["H2O"]    # , 'NH3'
 non_gas_sp = ["H2O_l_s"]
 
@@ -112,20 +114,20 @@ loss_eps = 1e-1
 yconv_cri = 0.01 # for checking steady-state
 slope_cri = 1.e-4
 yconv_min = 0.1
-flux_cri = 5.e-2 
+flux_cri = 0.1
 flux_atol = 1. # the tol for actinc flux (# photons cm-2 s-1 nm-1)
 # ====== Setting up numerical parameters for Ros2 ODE solver ====== 
-rtol = 0.1
+rtol = 0.25
 # ====== Setting up numerical parameters for SemiEu/SparSemiEU ODE solver (Not used) ====== 
 PItol = 0.1
 
 # ====== Setting up for output and plotting ======
 # plotting:
-plot_TP = True
+plot_TP = False
 use_live_plot = True
 use_live_flux = False
-use_plot_end = True
-use_plot_evo = True
+use_plot_end = False
+use_plot_evo = False
 use_save_movie = False
 use_flux_movie = False
 plot_height = False
@@ -133,7 +135,7 @@ use_PIL = True
 live_plot_frq = 10
 save_movie_rate = live_plot_frq
 y_time_freq = 1  #  storing data for every 'y_time_freq' step
-plot_spec = ['H', 'CH4', 'CO', 'CO2','C2H2', 'NH3', 'HCN']  
+plot_spec = ['H', 'CH4', 'CO', 'CO2','C2H2','HCN', 'NH3']  
 # output:
 output_humanread = False
 use_shark = False
